09438b2fdde3522708f1781588bada39ad50d756
Daniela Matos <daniela.lmatos@gmail.com>
2020-03-26 00:40:50 -0500

    Changed to new url

--- load.py
+++ load.py
@@ -10,7 +10,7 @@ countries_html = requests.get(countries_url).text
 countries_df = pd.read_html(countries_html)[0]
 
 # Covid-19 Confirmed Cases
-confirmed_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+confirmed_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 confirmed_html = requests.get(confirmed_url).text 
 confirmed_df = pd.read_html(confirmed_html)[0]
 confirmed_df = confirmed_df.iloc[:, 1:]
@@ -18,7 +18,7 @@ confirmed_df = confirmed_df.melt(id_vars=['Country/Region', 'Province/State', 'L
 confirmed_df = confirmed_df.rename(columns={"Country/Region": "country_region", "Province/State": "province_state", "variable":"date", "value": "confirmed"})
 
 # Covid-19 Deaths
-deaths_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+deaths_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 deaths_html = requests.get(deaths_url).text 
 deaths_df = pd.read_html(deaths_html)[0]
 deaths_df = deaths_df.iloc[:, 1:]
@@ -26,7 +26,7 @@ deaths_df = deaths_df.melt(id_vars=['Country/Region', 'Province/State', 'Lat', '
 deaths_df = deaths_df.rename(columns={"Country/Region": "country_region", "Province/State": "province_state", "variable":"date", "value": "deaths"})
 
 # Covid-19 Recovered
-recovered_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+recovered_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 recovered_html = requests.get(recovered_url).text 
 recovered_df = pd.read_html(recovered_html)[0]
 recovered_df = recovered_df.iloc[:, 1:]
@@ -37,62 +37,64 @@ recovered_df = recovered_df.rename(columns={"Country/Region": "country_region", 
 merged_df = pd.merge(confirmed_df, deaths_df)
 covid_df = pd.merge(merged_df, recovered_df)
 
-# Transform "Date" column into datatime format
-covid_df["date"] = pd.to_datetime(covid_df["date"])
+print(covid_df)
 
-### Add column ISO3 to "covid_df" ###
-covid_df1 = covid_df.loc[covid_df["country_region"] != "Cruise Ship"]
-cc = coco.CountryConverter()
-covid_df1 = covid_df1.replace(to_replace="UK", value="United Kingdom")
-country_list = list(covid_df1["country_region"])
-standard_names = cc.convert(names=country_list, to="name_short")
-covid_df1["iso3"] = cc.convert(names=standard_names, to="iso3")
-covid_df = pd.merge(covid_df, covid_df1, how="outer")
+# # Transform "Date" column into datatime format
+# covid_df["date"] = pd.to_datetime(covid_df["date"])
 
-## Summary table 
-# we are not sure if the most current date is today or yesterday.
-last_update = [covid_df["date"].max()]
-total_countries_infected = covid_df["iso3"].nunique()
-# Find the total number per date
-confirmed_byDate = covid_df.groupby("date").sum()["confirmed"]
-# The last value is the total world
-total_confirmed_world = confirmed_byDate[-1]
-# Find total deaths
-deaths_byDate = covid_df.groupby("date").sum()["deaths"]
-total_deaths_world = deaths_byDate[-1]
-# Find total deaths
-recovered_byDate = covid_df.groupby("date").sum()["recovered"]
-total_recovered_world = recovered_byDate[-1]
+# ### Add column ISO3 to "covid_df" ###
+# covid_df1 = covid_df.loc[covid_df["country_region"] != "Cruise Ship"]
+# cc = coco.CountryConverter()
+# covid_df1 = covid_df1.replace(to_replace="UK", value="United Kingdom")
+# country_list = list(covid_df1["country_region"])
+# standard_names = cc.convert(names=country_list, to="name_short")
+# covid_df1["iso3"] = cc.convert(names=standard_names, to="iso3")
+# covid_df = pd.merge(covid_df, covid_df1, how="outer")
 
-summary = pd.DataFrame(
-    {
-        "total_confirmed_world": total_confirmed_world,
-        "total_deaths_world": total_deaths_world,
-        "total_recovered_world": total_recovered_world,
-        "total_countries_infected": total_countries_infected,
-        "last_update": last_update,
-    }
-)
+# ## Summary table 
+# # we are not sure if the most current date is today or yesterday.
+# last_update = [covid_df["date"].max()]
+# total_countries_infected = covid_df["iso3"].nunique()
+# # Find the total number per date
+# confirmed_byDate = covid_df.groupby("date").sum()["confirmed"]
+# # The last value is the total world
+# total_confirmed_world = confirmed_byDate[-1]
+# # Find total deaths
+# deaths_byDate = covid_df.groupby("date").sum()["deaths"]
+# total_deaths_world = deaths_byDate[-1]
+# # Find total deaths
+# recovered_byDate = covid_df.groupby("date").sum()["recovered"]
+# total_recovered_world = recovered_byDate[-1]
 
-# Aggregate by county
-# confirmed_by_country_df = covid_df.groupby(["date", "country"])["confirmed"].sum().to_frame().reset_index()
+# summary = pd.DataFrame(
+#     {
+#         "total_confirmed_world": total_confirmed_world,
+#         "total_deaths_world": total_deaths_world,
+#         "total_recovered_world": total_recovered_world,
+#         "total_countries_infected": total_countries_infected,
+#         "last_update": last_update,
+#     }
+# )
 
-# Connect to the "Covid" database in MySQL (CHANGE PASSWORD)
-HOSTNAME = "127.0.0.1"
-PORT = 3306
-USERNAME = "root"
-PASSWORD = "root"
-DIALECT = "mysql"
-DRIVER = "pymysql"
-DATABASE = "Covid"
+# Aggregate by country
+# # confirmed_by_country_df = covid_df.groupby(["date", "country"])["confirmed"].sum().to_frame().reset_index()
 
-connection_string = ( f"{DIALECT}+{DRIVER}://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}" )
-engine = create_engine(connection_string)
+# # Connect to the "Covid" database in MySQL (CHANGE PASSWORD)
+# HOSTNAME = "127.0.0.1"
+# PORT = 3306
+# USERNAME = "root"
+# PASSWORD = "Amd230313"
+# DIALECT = "mysql"
+# DRIVER = "pymysql"
+# DATABASE = "Covid"
 
-# Create "daily_cases" table in "Covid" database with "covid_db" dataframe
-covid_df.to_sql(con=engine, name="daily_cases", if_exists="replace")
-summary.to_sql(con=engine, name="summary", if_exists="replace")
-countries_df.to_sql(con=engine, name="countries", if_exists="replace")
+# connection_string = ( f"{DIALECT}+{DRIVER}://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}" )
+# engine = create_engine(connection_string)
+
+# # Create "daily_cases" table in "Covid" database with "covid_db" dataframe
+# covid_df.to_sql(con=engine, name="daily_cases", if_exists="replace")
+# # summary.to_sql(con=engine, name="summary", if_exists="replace")
+# countries_df.to_sql(con=engine, name="countries", if_exists="replace")
 
 # engine.execute(
 #     "CREATE table world_timeseries (id INT NOT NULL auto_increment, date datetime, total_confirmed bigint(20), total_deaths bigint(20), total_recovered bigint(20), primary key (id)) SELECT date, SUM(confirmed) as total_confirmed, SUM(deaths) as total_deaths, SUM(recovered) as total_recovered FROM Covid.daily_cases GROUP BY date"
