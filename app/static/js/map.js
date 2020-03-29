fa5db5785b012dabf64c919b6b9c0d6f4a727da7
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-03-29 12:38:57 -0500

    Added plotly code to be integrated with data

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -49,7 +49,32 @@ d3.json(`/records/`).then( data => {
 });
 
 
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2010_alcohol_consumption_by_country.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
 
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'ISO-3',
+        locations: unpack(rows, 'location'),
+        z: unpack(rows, 'alcohol'),
+        text: unpack(rows, 'location'),
+        autocolorscale: true
+    }];
+
+    var layout = {
+      title: 'Pure alcohol consumption<br>among adults (age 15+) in 2010',
+      geo: {
+          projection: {
+              type: 'robinson'
+          }
+      }
+    };
+
+    Plotly.newPlot("map-container", data, layout, {showLink: false});
+
+});
 
 
 
