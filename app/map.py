734c1f84a1345862c56a0c7156ecf276d26bfe05
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-04-06 20:57:46 -0500

    map slider fully functional...very very very slow. Also need to add date to slider scale.

--- app/map.py
+++ app/map.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 import plotly.offline
 import pandas as pd
 import datetime
-
+import os
 #%%
 raw_df = pd.read_json(path_or_buf = "http://127.0.0.1:5000/records/", orient="records", convert_dates=True)
 raw_df["date"] = raw_df["date"].apply(lambda x: x.strftime('%Y-%m-%d'))
@@ -23,18 +23,40 @@ def create_traces(fig, df, confirmed_died_recovered):
         fig.add_trace(
             go.Choropleth(
                 locations = trace_df["iso3"],
+                locationmode = "ISO-3",
                 z = trace_df[confirmed_died_recovered],
                 text = f"{confirmed_died_recovered.title()}",
                 autocolorscale=True,
                 customdata=[date]  
             ),
         )
+
+    fig.update_layout(
+        title={
+        "text": f"COVID-19 {confirmed_died_recovered.title()} Cases as of {max(all_dates)}",
+        "y":0.95,
+        "x":0.5,
+        "xanchor": "center",
+        "yanchor": "top"},
+        autosize=True,
+        geo= {
+		    "scope": "world",
+		    "projection": {
+			    "type": 'natural earth'
+		    },
+		    "oceancolor": '#3399ff',
+		    "showcountries": True,
+	    }
+    )
+
+
+
 create_traces(confirmed_fig, raw_df, "confirmed")
 create_traces(deaths_fig, raw_df, "deaths")
 create_traces(recovered_fig, raw_df, "recovered")
 
 #%%
-def create_slider(fig, most_recent, confirmed_died_recovered):
+def create_slider(fig, confirmed_died_recovered):
     steps = []
     for i in range(len(fig.data)):
         step = dict(
@@ -59,24 +81,22 @@ def create_slider(fig, most_recent, confirmed_died_recovered):
         )
     ]
 
-    map_fig.update_layout(
-        title={
-        'text': f'COVID-19 {confirmed_died_recovered.title()} Cases (as of ({most_recent})',
-        'y':0.95,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'},
+    fig.update_layout(
         sliders=sliders
     )
 
-create_slider(confirmed_fig, max_date, "confirmed")
-create_slider(deaths_fig, max_date, "deaths")
-create_slider(recovered_fig, max_date, "recovered")
+create_slider(confirmed_fig, "confirmed")
+# create_slider(deaths_fig, max_date, "deaths")
+# create_slider(recovered_fig, max_date, "recovered")
 
 
 
 #%%
-map_div = plotly.offline.plot(map_fig, include_plotlyjs=False, output_type='div')
+confirmed_div = plotly.offline.plot(confirmed_fig, include_plotlyjs=False, output_type='div')
 
+
+# %%
+with open("file.txt", "w") as write_file:
+    write_file.write(confirmed_div)
 
 # %%
